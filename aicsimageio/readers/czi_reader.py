8b312cafc03950907ffbbb0cbd5d4deb4a916d35
Jamie Sherman <jamie.sherman@gmail.com>
2019-06-12 15:55:40 -0700

    90% of CziReader

--- aicsimageio/readers/czi_reader.py
+++ aicsimageio/readers/czi_reader.py
@@ -1,12 +1,23 @@
+import czifile
+import io
+import logging
 import numpy as np
+from typing import Union
 import warnings
+import xml
+
+from aicsimageio import types
+from .reader import Reader
 
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
     from aicsimageio.vendor import czifile
 
+log = logging.getLogger(__name__)
+CZI_NATIVE_ARRAY = np.ndarray
 
-class CziReader:
+
+class CziReader(Reader):
     """This class is used primarily for opening and processing the contents of a CZI file
 
     Example:
@@ -46,12 +57,43 @@ class CziReader:
     '0' is the numbers of channels per pixel (always =zero for our data)
     """
 
-    def __init__(self, file_path, max_workers=None):
+    @staticmethod
+    def _is_this_type(byte_io: io.BytesIO) -> bool:
+        pass
+
+    @property
+    def data(self) -> np.ndarray:
+        """
+        Returns
+        -------
+        the data from the czi file with the native order (i.e. "TZCYX")
+        """
+        return self._lazy_init_data()
+
+    @property
+    def dims(self) -> str:
+        """
+        Returns
+        -------
+        The native shape of the image.
+        """
+        return self.czi.axes
+
+    @property
+    def metadata(self) -> xml.etree.ElementTree :
+        return self._lazy_init_metadata()
+
+    def __init__(self, file: Union[types.PathLike, types.BytesLike], max_workers=None):
         """
         :param file_path(str): The path for the file that is to be opened.
         """
-        self.filePath = file_path
-        self.czi = czifile.CziFile(self.filePath)
+        super().__init__(file)
+        try:
+            self.czi = czifile.CziFile(self._bytes)
+        except Exception:
+            log.error("czifile could not parse this input")
+            raise
+
         self.hasTimeDimension = 'T' in self.czi.axes
         self._max_workers = max_workers
 
@@ -64,103 +106,31 @@ class CziReader:
     def close(self):
         self.czi.close()
 
-    def load(self):
-        """Retrieves an array for all z-slices and channels.
-
-        :return: 5D array with dimensions TZCYX.
+    def _lazy_init_data(self) -> CZI_NATIVE_ARRAY:
+        """Retrieves an array of the image file
+        :return:
         """
-        image = self.czi.asarray(max_workers=self._max_workers)
+        if self._data is None:
+            # load the data
+            self._data = self.czi.asarray(max_workers=self._max_workers)
+        return self._data
 
-        # TODO: Proper error checking if indices are incorrect
-        axes = self.czi.axes
-        assert(len(image.shape) == len(axes))
-
-        knowndims = 'TZCYX'
-        axisordering = []
-        # We want to strip all dimensions away that are not in knowndims
-        # and then transpose into the ordering contained in knowndims.
-        # Build up a slice tuple like [0,0,:,:,:,0]
-        # Start by taking the first element from each axis
-        slicing = [0]*len(axes)
-        # now find the index of axes we care about and take the whole data set of that axis using slice(None)
-        missing_axes = []
-        for i in range(len(knowndims)):
-            pos = axes.find(knowndims[i])
-            if pos != -1:
-                axisordering.append(pos)
-                slicing[pos] = slice(None)
-            else:
-                missing_axes.append(i)
-
-        # axisordering contains indices of dimensions in the original array.
-        # convert to dimensions in the new sliced array.
-        # something like [3 2 4 5] turns into [1 0 2 3]
-        axisordering = sorted(range(len(axisordering)), key=axisordering.__getitem__)
-
-        transposed_image = image[tuple(slicing)]
-        # don't assume all known dimensions are present: time might be missing?
-        # assert(len(transposed_image.shape) == len(knowndims))
-        assert(len(transposed_image.shape) == len(axisordering))
-        transposed_image = np.transpose(transposed_image, tuple(axisordering))
-
-        for i in missing_axes:
-            transposed_image = np.expand_dims(transposed_image, i)
-
-        return transposed_image
-
-    def load_slice(self, z=0, c=0, t=0):
-        """Retrieves the 2D YX slice from the image
-
-        :param z: The z index that will be accessed
-        :param c: The channel that will be accessed
-        :param t: The time index that will be accessed
-        :return: 2D array with dimensions YX
+    def _lazy_init_metadata(self) -> xml.etree.ElementTree:
         """
-        image_slice = []
-        if self.hasTimeDimension:
-            for directory_entry in self.czi.filtered_subblock_directory:
-                if directory_entry.start[3] == z and directory_entry.start[2] == c and directory_entry.start[1] == t:
-                    # tile is a slice with all seven dimensions (BTCZYX0) still intact
-                    tile = directory_entry.data_segment().data()
-                    image_slice = tile[0, 0, 0, 0, :, :, 0]
-                    return image_slice
-        else:
-            for directory_entry in self.czi.filtered_subblock_directory:
-                if directory_entry.start[2] == z and directory_entry.start[1] == c:
-                    # tile is a slice with all six dimensions (BCZYX0) still intact
-                    tile = directory_entry.data_segment().data()
-                    if len(tile.shape) == 6:
-                        image_slice = tile[0, 0, 0, :, :, 0]
-                    elif len(tile.shape) == 5:
-                        # No z dimension
-                        image_slice = tile[0, 0, :, :, 0]
-                    else:
-                        # Throw an exception
-                        raise DimensionNotSupportedError("The file dimension is not supported")
-                    return image_slice
+        Lazy load the metadata from the CZI file
+        Returns
+        -------
+        The xml Element Tree of the metadata
+        """
+        if self._metadata is None:
+            # load the metadata
+            self._metadata = self.czi.metadata
+        return self._metadata
 
-    def get_metadata(self):
-        return self.czi.metadata
-
-    def size_z(self):
-        return self.czi.shape[3] if self.hasTimeDimension else self.czi.shape[2]
-
-    def size_c(self):
-        return self.czi.shape[2] if self.hasTimeDimension else self.czi.shape[1]
-
-    def size_t(self):
-        return self.czi.shape[1] if self.hasTimeDimension else 1
-
-    def size_x(self):
-        return self.czi.shape[5] if self.hasTimeDimension else self.czi.shape[4]
-
-    def size_y(self):
-        return self.czi.shape[4] if self.hasTimeDimension else self.czi.shape[3]
-
-    def dtype(self):
+    def dtype(self) -> np.dtype:
+        """
+        Returns
+        -------
+        the data type of the ndarray being returned (uint16, uint8, etc)
+        """
         return self.czi.dtype
-
-
-class DimensionNotSupportedError(Exception):
-    """Exception to indicate that the dimension of the file is not supported by this reader."""
-    pass
---
+++ aicsimageio/tests/readers/test_czi_reader.py
@@ -0,0 +1,9 @@
+import pytest
+from aicsimageio.readers.czi_reader import CziReader
+from czifile import CziFile
+
+
+def test_czifile():
+    czi = CziFile("/Users/jamies/20180907_M01_001.czi")
+    print(f"axes:= {czi.axes}")
+

b1638c871e01bf86d08c20f25c9315c8eb5c9389
Matthew Bowden <bowdenm@spu.edu>
2019-06-12 14:47:26 -0700

    Add TiffReader type check tests

--- aicsimageio/tests/testTifReader.py
+++ aicsimageio/tests/testTifReader.py
@@ -2,10 +2,26 @@
 import math
 import os
 import unittest
+from pathlib import Path
 
 import numpy as np
+import pytest
 
 from aicsimageio.readers import TiffReader
+
+
+RESOURCES = Path(__file__).parent / 'img'
+
+
+@pytest.mark.parametrize('name, expected', [
+    ('img40_1_dna.tif', True),
+    ('TestXYCZ_imagej.tif', True),
+    ('img40_1.png', False),
+    ('single-channel.ome.tif', True)
+])
+def test_type_check(name, expected):
+    tiff_image = RESOURCES / name
+    assert TiffReader.is_this_type(tiff_image) == expected
 
 
 class TestTifReader(unittest.TestCase):

de40dcb23c67cc8cbdb4307e7cde51f74873b3ef
Jamie Sherman <jamie.sherman@gmail.com>
2019-06-14 01:31:38 -0700

    improved test coverage

--- aicsimageio/tests/readers/test_czi_reader.py
+++ aicsimageio/tests/readers/test_czi_reader.py
@@ -14,7 +14,8 @@ import unittest
     ]
 )
 def test_czi_reader(file):
-    CziReader(file)
+    czi = CziReader(file)
+    czi.close()
 
 
 @pytest.mark.parametrize("test_input,expected", [
@@ -29,20 +30,46 @@ def test_is_this_type(test_input, expected):
     assert res == expected
 
 
-def test_dims():
-    czi = CziReader("/Users/jamies/20180907_M01_001.czi")
-    assert czi.dims == "BSTCZYX0"
+@pytest.mark.parametrize("test_input,expected", [
+    ("test_5_dimension.czi", "BCYX0"),
+    ("T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi", "BTCZYX0")
+])
+def test_dims(image_dir, test_input, expected):
+    czi = CziReader(image_dir / test_input)
+    assert czi.dims == expected
 
 
-def test_shape():
-    czi = CziReader("img/test_5_dimensions.czi")
+@pytest.mark.parametrize("test_input,expected", [
+    ("test_5_dimension.czi", 'uint16'),
+    ("T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi", 'uint8')
+])
+def test_dtype(image_dir, test_input, expected):
+    czi = CziReader(image_dir / test_input)
+    assert czi.dtype() == expected
+
+
+def test_shape(image_dir):
+    czi = CziReader(image_dir / "T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi")
     data = czi.data
     data_shape = data.shape
     assert data_shape[0] == 1
-    assert data_shape[1] == 1
-    assert data_shape[2] == czi.size_t()
-    assert data_shape[3] == czi.size_c()
-    assert data_shape[4] == czi.size_z()
-    assert data_shape[5] == czi.size_y()
-    assert data_shape[6] == czi.size_x()
+    assert data_shape[1] == czi.size_t()
+    assert data_shape[2] == czi.size_c()
+    assert data_shape[3] == czi.size_z()
+    assert data_shape[4] == czi.size_y()
+    assert data_shape[5] == czi.size_x()
     assert czi._is_multiscene() is False
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ('T', 1), ('C', 2), ('Z', 3), ('Y', 4), ('X', 5), ('S', -1), ('B', 0), ('M', -1), ('V', -1),
+    pytest.param("TZ", False, marks=pytest.mark.raises(exception=TypeError))
+])
+def test_lookup_index(image_dir, test_input, expected):
+    czi = CziReader(image_dir / 'T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi')
+    assert czi._lookup_dimension_index(test_input) == expected
+
+
+def test_missing_dimension(image_dir):
+    czi = CziReader(image_dir / 'T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi')
+    assert czi._size_of_dimension('V') == 0

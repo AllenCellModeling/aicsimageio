e4be771ed5511650f3ef29c9a57a93b203d5dac5
Dan Toloudis <danielt@alleninstitute.org>
2019-06-15 11:41:49 -0700

    fill in TypeChecker implementation

--- aicsimageio/type_checker.py
+++ aicsimageio/type_checker.py
@@ -1,4 +1,9 @@
 
+from . import types
+from .readers.czi_reader import CziReader
+from .readers.ome_tiff_reader import OmeTiffReader
+from .readers.tiff_reader import TiffReader
+
 
 class TypeChecker:
     """Cheaply check to see if a given file is a recognized type.
@@ -6,4 +11,8 @@ class TypeChecker:
     If the file is a TIFF, then the description (OME XML if it is OME TIFF) can be retrieved via read_description.
     Similarly, if the file is a CZI, then the metadata XML can be retrieved via read_description.
     """
-    pass
+    def __init__(self, file: types.Union[types.PathLike, types.BufferLike]):
+        self.is_czi = CziReader.is_this_type(file)
+        self.is_tiff = TiffReader.is_this_type(file)
+        self.is_ome = OmeTiffReader.is_this_type(file)
+

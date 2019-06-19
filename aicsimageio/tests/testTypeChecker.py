98263b8864b19e4c207653f8c832389547f9f2e3
Matthew Bowden <bowdenm@spu.edu>
2019-06-19 14:17:36 -0700

    Update type checker tests

--- aicsimageio/tests/testTypeChecker.py
+++ aicsimageio/tests/testTypeChecker.py
@@ -1,42 +1,16 @@
-import unittest
-import os
+import pytest
 
-from aicsimageio import TypeChecker
+from aicsimageio.aics_image import AICSImage
+from aicsimageio import readers
 
 
-class TestTypeChecker(unittest.TestCase):
-
-    def setUp(self):
-        unittest.TestCase.__init__(self)
-
-    def runTest(self):
-        # this method has to be included in a testgroup in order for it be run
-        self.assertTrue(True)
-
-    def test_png(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        checker = TypeChecker(os.path.join(dir_path, 'img', 'img40_1.png'))
-        self.assertFalse(checker.is_czi)
-        self.assertFalse(checker.is_tiff)
-        self.assertFalse(checker.is_ome)
-
-    def test_tiff(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        checker = TypeChecker(os.path.join(dir_path, 'img', 'img40_1_dna.tif'))
-        self.assertFalse(checker.is_czi)
-        self.assertTrue(checker.is_tiff)
-        self.assertFalse(checker.is_ome)
-
-    def test_ome_tiff(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        checker = TypeChecker(os.path.join(dir_path, 'img', 'img40_1.ome.tif'))
-        self.assertFalse(checker.is_czi)
-        self.assertTrue(checker.is_tiff)
-        self.assertTrue(checker.is_ome)
-
-    def test_czi(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        checker = TypeChecker(os.path.join(dir_path, 'img', 'T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi'))
-        self.assertTrue(checker.is_czi)
-        self.assertFalse(checker.is_tiff)
-        self.assertFalse(checker.is_ome)
+# TODO: Move to AICSImage tests
+@pytest.mark.parametrize('filename, expected_reader', [
+    ('img40_1.png', readers.DefaultReader),
+    ('img40_1_dna.tif', readers.TiffReader),
+    ('img40_1.ome.tif', readers.OmeTiffReader),
+    ('T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi', readers.CziReader),
+])
+def test_typing(filename, expected_reader, image_dir):
+    actual_reader = AICSImage.determine_reader(image_dir / filename)
+    assert actual_reader == expected_reader

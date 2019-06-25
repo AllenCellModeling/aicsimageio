1d4101d01439f396874401f51caa668a4bc1c072
Jamie Sherman <jamie.sherman@gmail.com>
2019-06-25 15:40:21 -0700

    added test for values as recommended by dan

--- aicsimageio/tests/testTransforms.py
+++ aicsimageio/tests/testTransforms.py
@@ -5,7 +5,7 @@ from aicsimageio.exceptions import ConflictingArgsError
 
 
 # test _reshape_data which is called when data is accessed
-@pytest.mark.parametrize("kwargs,expected", [
+@pytest.mark.parametrize("kwargs, expected", [
     ({'data': np.zeros((10, 1, 5, 6, 200, 400)), 'given_dims': "STCZYX", 'return_dims': "CSZYX"}, (5, 10, 6, 200, 400)),
     ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "STCZYX"}, (1, 1, 1, 6, 200, 400)),
     ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "ZCYSXT"}, (6, 1, 200, 1, 400, 1)),
@@ -16,9 +16,25 @@ from aicsimageio.exceptions import ConflictingArgsError
     pytest.param({'data': np.zeros((6, 200, 400)), 'given_dims': 'ZYX', 'return_dims': 'TYXCZX', 'Z': 7}, 5,
                  marks=pytest.mark.raises(exception=ConflictingArgsError)),
 ])
-def test_reshape_data(kwargs, expected):
+def test_reshape_data_shape(kwargs, expected):
     ans = reshape_data(**kwargs)
     assert ans.shape == expected
+
+
+@pytest.mark.parametrize("kwargs,idx_in,idx_out", [
+    ({'data': np.random.rand(10, 1, 5, 6, 200, 400), 'given_dims': "STCZYX", 'return_dims': "CSZYX"},
+     (5, 0, 3, 3, ...), (3, 5, 3, ...)),
+    ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "STCZYX"},
+     (..., 100, 200), (0, 0, 0, ..., 100, 200)),
+    ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "ZCYSXT"},
+     (3, 100, ...), (3, 0, 100, 0, ..., 0)),
+])
+def test_reshape_data_values(kwargs, idx_in, idx_out):
+    data = kwargs['data']
+    slice_in = data[idx_in]
+    ans = reshape_data(**kwargs)
+    for (a, b) in zip(slice_in.flat, ans[idx_out].flat):
+        assert a == b
 
 
 @pytest.mark.parametrize("kwargs,expected", [

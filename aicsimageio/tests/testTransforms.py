d2b306be3e4fb1966acf6a4ea9159db2aa89a5ed
Jamie Sherman <jamie.sherman@gmail.com>
2019-06-26 13:08:14 -0700

    changed kwargs in tests to explicit arguments

--- aicsimageio/tests/testTransforms.py
+++ aicsimageio/tests/testTransforms.py
@@ -5,50 +5,43 @@ from aicsimageio.exceptions import ConflictingArgumentsError
 
 
 # test _reshape_data which is called when data is accessed
-@pytest.mark.parametrize("input_dict, expected", [
-    ({'data': np.zeros((10, 1, 5, 6, 200, 400)), 'given_dims': "STCZYX", 'return_dims': "CSZYX"}, (5, 10, 6, 200, 400)),
-    ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "STCZYX"}, (1, 1, 1, 6, 200, 400)),
-    ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "ZCYSXT"}, (6, 1, 200, 1, 400, 1)),
-    pytest.param({'data': np.zeros((6, 200, 400)), 'given_dims': 'ZYX', 'return_dims': 'TYXC', 'Z': 7}, 5,
+@pytest.mark.parametrize("data, given_dims, return_dims, other_args, expected", [
+    (np.zeros((10, 1, 5, 6, 200, 400)), "STCZYX", "CSZYX", {}, (5, 10, 6, 200, 400)),
+    (np.zeros((6, 200, 400)), "ZYX", "STCZYX", {}, (1, 1, 1, 6, 200, 400)),
+    (np.zeros((6, 200, 400)), "ZYX", "ZCYSXT", {}, (6, 1, 200, 1, 400, 1)),
+    pytest.param(np.zeros((6, 200, 400)), 'ZYX', 'TYXC', {'Z': 7}, 5,
                  marks=pytest.mark.raises(exception=IndexError)),
-    pytest.param({'data': np.zeros((6, 200, 400)), 'given_dims': 'ZYX', 'return_dims': 'TYXCZ', 'Z': 7}, 5,
+    pytest.param(np.zeros((6, 200, 400)), 'ZYX', 'TYXCZ', {'Z': 7}, 5,
                  marks=pytest.mark.raises(exception=ConflictingArgumentsError)),
-    pytest.param({'data': np.zeros((6, 200, 400)), 'given_dims': 'ZYX', 'return_dims': 'TYXCZX', 'Z': 7}, 5,
+    pytest.param(np.zeros((6, 200, 400)), 'ZYX', 'TYXCZX', {'Z': 7}, 5,
                  marks=pytest.mark.raises(exception=ConflictingArgumentsError)),
 ])
-def test_reshape_data_shape(input_dict, expected):
-    # since transpose to dims has keywords for all arguments I'm using a dictionary
-    # and expanding it with the ** operator
-    ans = reshape_data(**input_dict)
+def test_reshape_data_shape(data, given_dims, return_dims, other_args, expected):
+    # the other_args are being used to pass slice specific information by expanding with the ** operator
+    ans = reshape_data(data=data, given_dims=given_dims, return_dims=return_dims, **other_args)
     assert ans.shape == expected
 
 
-@pytest.mark.parametrize("input_dict, idx_in, idx_out", [
-    ({'data': np.random.rand(10, 1, 5, 6, 200, 400), 'given_dims': "STCZYX", 'return_dims': "CSZYX"},
-     (5, 0, 3, 3, ...), (3, 5, 3, ...)),
-    ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "STCZYX"},
-     (..., 100, 200), (0, 0, 0, ..., 100, 200)),
-    ({'data': np.zeros((6, 200, 400)), 'given_dims': "ZYX", 'return_dims': "ZCYSXT"},
-     (3, 100, ...), (3, 0, 100, 0, ..., 0)),
+@pytest.mark.parametrize("data, given_dims, return_dims, idx_in, idx_out", [
+    (np.random.rand(10, 1, 5, 6, 200, 400), "STCZYX", "CSZYX", (5, 0, 3, 3, ...), (3, 5, 3, ...)),
+    (np.zeros((6, 200, 400)), "ZYX", "STCZYX", (..., 100, 200), (0, 0, 0, ..., 100, 200)),
+    (np.zeros((6, 200, 400)), "ZYX", "ZCYSXT", (3, 100, ...), (3, 0, 100, 0, ..., 0)),
 ])
-def test_reshape_data_values(input_dict, idx_in, idx_out):
-    # for explanation of input_dict see first use above
-    data = input_dict['data']
+def test_reshape_data_values(data, given_dims, return_dims, idx_in, idx_out):
     slice_in = data[idx_in]
-    ans = reshape_data(**input_dict)
+    ans = reshape_data(data=data, given_dims=given_dims, return_dims=return_dims)
     for a, b in zip(slice_in.flat, ans[idx_out].flat):
         assert a == b
 
 
-@pytest.mark.parametrize("input_dict, expected", [
-    ({'data': np.zeros((1, 2, 3, 4, 5, 6)), 'given_dims': "STCZYX", 'return_dims': "XYZCTS"}, (6, 5, 4, 3, 2, 1)),
-    ({'data': np.zeros((1, 2, 3)), 'given_dims': "ZYX", 'return_dims': "ZXY"}, (1, 3, 2)),
-    pytest.param({'data': np.zeros((6, 200, 400)), 'given_dims': 'ZYX', 'return_dims': 'TYXC'}, 5,
+@pytest.mark.parametrize("data, given_dims, return_dims, expected", [
+    (np.zeros((1, 2, 3, 4, 5, 6)), "STCZYX", "XYZCTS", (6, 5, 4, 3, 2, 1)),
+    (np.zeros((1, 2, 3)), "ZYX", "ZXY", (1, 3, 2)),
+    pytest.param(np.zeros((6, 200, 400)), 'ZYX', 'TYXC', 5,
                  marks=pytest.mark.raises(exception=ConflictingArgumentsError)),
-    pytest.param({'data': np.zeros((6, 200, 400)), 'given_dims': 'ZYX', 'return_dims': 'TYXCZ'}, 5,
+    pytest.param(np.zeros((6, 200, 400)), 'ZYX', 'TYXCZ', 5,
                  marks=pytest.mark.raises(exception=ConflictingArgumentsError)),
 ])
-def test_transpose_to_dims(input_dict, expected):
-    # for explanation of input_dict see first use above
-    data = transpose_to_dims(**input_dict)
+def test_transpose_to_dims(data, given_dims, return_dims, expected):
+    data = transpose_to_dims(data=data, given_dims=given_dims, return_dims=return_dims)
     assert data.shape == expected

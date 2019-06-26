2523c8ce57a12a3ad40dd1934a244f5ccec3c801
Jamie Sherman <jamie.sherman@gmail.com>
2019-06-25 23:40:02 -0700

    updated tests to be cleaner

--- aicsimageio/tests/testAICSImage.py
+++ aicsimageio/tests/testAICSImage.py
@@ -1,42 +1,9 @@
-import random
-
 import numpy as np
 import pytest
-from aicsimageio import AICSImage, readers
+from xml.etree import cElementTree as etree
 
-
-class ImgContainer(object):
-    def __init__(self, channels: int = 5, dims: str = "TCZYX"):
-        self.input_shape = random.sample(range(1, 10), channels)
-        stack = np.zeros(self.input_shape)
-        self.dims = dims
-        self.order = {
-            c: i for i, c in enumerate(dims)
-        }  # {'T': 0, 'C': 1, 'Z': 2, 'Y': 3, 'X': 4}
-        self.image = AICSImage(stack, dims=self.dims)
-
-    def remap(self, seq):
-        return [self.order[c] for c in seq]
-
-    def shuffle_shape(self, seq):
-        new_shape = [self.input_shape[self.order[c]] for c in seq]
-        return tuple(new_shape)
-
-    def get_trand_crand(self):
-        tmax = self.input_shape[self.order["T"]]
-        cmax = self.input_shape[self.order["C"]]
-        trand, crand = random.randint(0, tmax - 1), random.randint(0, cmax - 1)
-        return trand, crand
-
-
-@pytest.fixture
-def example_img5():
-    return ImgContainer()
-
-
-@pytest.fixture
-def example_img3ctx():
-    return ImgContainer(3, "CTX")
+from aicsimageio import AICSImage, readers, exceptions
+from aicsimageio.vendor import omexml
 
 
 @pytest.mark.parametrize('filename, expected_reader', [
@@ -44,57 +11,50 @@ def example_img3ctx():
     ('img40_1_dna.tif', readers.TiffReader),
     ('img40_1.ome.tif', readers.OmeTiffReader),
     ('T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi', readers.CziReader),
-    pytest.param('not/a/file.czi', None, marks=pytest.mark.raises(exception=FileNotFoundError))
+    pytest.param('not/a/file.czi', None, marks=pytest.mark.raises(exception=FileNotFoundError)),
 ])
 def test_typing(filename, expected_reader, image_dir):
     actual_reader = AICSImage.determine_reader(image_dir / filename)
     assert actual_reader == expected_reader
 
 
+@pytest.mark.skip(reason="exception state is unreachable")
+def test_unsupported_typing():
+    with pytest.raises(exceptions.UnsupportedFileFormatError):
+        AICSImage.determine_reader(data=b"")
+
+
 @pytest.mark.parametrize('arr', [
     np.zeros((2, 2, 2)),
-    np.ones((2, 2, 2))
+    np.ones((2, 2, 2)),
+    np.random.rand(2, 2, 2)
 ])
 def test_support_for_ndarray(arr):
     actual_reader = AICSImage.determine_reader(arr)
     assert actual_reader == readers.NdArrayReader
 
 
-@pytest.mark.skip("Failing on my branch, Jamie wrote them, so handing back to Jamie")
-def test_helper_class(example_img5):
-    assert example_img5.remap("XYZCT") == [4, 3, 2, 1, 0]
+@pytest.mark.parametrize("data,expected", [
+    (np.zeros((5, 4, 3)), (1, 1, 1, 5, 4, 3)),
+    (np.zeros((1, 2, 3, 4, 5, 6)), (1, 2, 3, 4, 5, 6)),
+    (np.random.rand(10), (1, 1, 1, 1, 1, 10))
+])
+def test_aicsimage_default_dims(data, expected):
+    img = AICSImage(data=data)
+    assert img.data.shape == expected
 
 
-@pytest.mark.skip("Failing on my branch, Jamie wrote them, so handing back to Jamie")
-def test_transposed_output(example_img5):
-    output_array = example_img5.image.get_image_data("XYZCT")
-    stack_shape = example_img5.shuffle_shape("XYZCT")
-    assert output_array.shape == stack_shape
-
-
-@pytest.mark.skip("Failing on my branch, Jamie wrote them, so handing back to Jamie")
-def test_transpose(example_img5):
-    output_array = AICSImage._AICSImage__transpose(
-        example_img5.image.data, example_img5.dims, "YZXCT"
-    )
-    stack_shape = example_img5.shuffle_shape("YZXCT")
-    assert output_array.shape == stack_shape
-
-
-@pytest.mark.skip(reason='Waiting on ndarray support in DefaultReader')
-def test_transposed_output_2(example_img5):
-    order = "TCZYX"
-    for _ in range(0, 20):
-        new_order = "".join(random.sample(order, len(order)))
-        image = example_img5.image.get_image_data(new_order)
-        shape = example_img5.shuffle_shape(new_order)
-        assert image.shape == shape
-
-
-@pytest.mark.skip(reason='Waiting on ndarray support in DefaultReader')
-def test_get_image_data_small_data(example_img3ctx):
-    image = example_img3ctx.image.get_image_data("TCX")
-    assert image.shape == example_img3ctx.shuffle_shape("TCX")
+@pytest.mark.parametrize("data_shape,dims,expected", [
+    ((5, 4, 3), "STC", (5, 4, 3, 1, 1, 1)),
+    ((1, 2, 3, 4, 5, 6), "XYZCTS", (6, 5, 4, 3, 2, 1)),
+    ((5, 4, 3), "SCY", (5, 1, 4, 1, 3, 1)),
+    ((1, 2, 3, 4, 5, 6), "STCZYX", (1, 2, 3, 4, 5, 6))
+])
+def test_aicsimage_force_dims(data_shape, dims, expected):
+    img = AICSImage(data=np.zeros(data_shape))
+    img._reader.dims = dims
+    assert img.data.shape == expected
+    assert data_shape == img.get_image_data(out_orientation=dims).shape
 
 
 @pytest.mark.parametrize("filepath", [
@@ -116,3 +76,27 @@ def test_file_passed_was_byte_string(image_dir):
     with pytest.raises(ValueError):
         img = AICSImage(b"not-a-valid-image-byte-array")
         img.data
+
+
+@pytest.mark.parametrize('filename, expected_metadata_type', [
+    ('img40_1.png', (str, type(None))),
+    ('img40_1_dna.tif', (str, type(None))),
+    ('img40_1.ome.tif', (str, omexml.OMEXML)),
+    ('T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi', (str, etree.Element))
+])
+def test_metadata(filename, expected_metadata_type, image_dir):
+    img = AICSImage(image_dir / filename)
+    print(f"class: {img.metadata.__class__.__name__}")
+    assert isinstance(img.metadata, expected_metadata_type)
+
+
+@pytest.mark.parametrize('filename, expected_reader', [
+    ('img40_1.png', readers.DefaultReader),
+    ('img40_1_dna.tif', readers.TiffReader),
+    ('img40_1.ome.tif', readers.OmeTiffReader),
+    ('T=5_Z=3_CH=2_CZT_All_CH_per_Slice.czi', readers.CziReader),
+    pytest.param('not/a/file.czi', None, marks=pytest.mark.raises(exception=FileNotFoundError)),
+])
+def test_reader(filename, expected_reader, image_dir):
+    img = AICSImage(image_dir / filename)
+    assert isinstance(img.reader, expected_reader)

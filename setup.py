535bf7bfb45da20bb315f7c5baa948aa4d095aea
Jackson Brown <jmaxfieldbrown@gmail.com>
2019-06-27 20:13:01 -0700

    [gh-1] Add external cookiecutter build procedure configuration files

--- setup.py
+++ setup.py
@@ -1,54 +1,111 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+"""The setup script."""
 
-PACKAGE_NAME = 'aicsimageio'
+from setuptools import find_packages, setup
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
 
-"""
-Notes:
-We get the constants MODULE_VERSION from
-See (3) in following link to read about versions from a single source
-https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
-"""
+requirements = [
+    "numpy>=1.16",
+    "imageio>=2.3.0",
+    "scipy>=1.1.0",
+    "tifffile==0.15.1",
+]
 
-MODULE_VERSION = ""
-exec(open(PACKAGE_NAME + "/version.py").read())
+test_requirements = [
+    "codecov",
+    "flake8",
+    "pytest",
+    "pytest-cov",
+    "pytest-raises",
+]
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+dev_requirements = [
+    "bumpversion>=0.5.3",
+    "coverage>=5.0a4",
+    "flake8>=3.7.7",
+    "ipython>=7.5.0",
+    "pytest>=4.3.0",
+    "pytest-cov==2.6.1",
+    "pytest-raises>=0.10",
+    "pytest-runner>=4.4",
+    "Sphinx>=2.0.0b1",
+    "tox>=3.5.2",
+    "twine>=1.13.0",
+    "wheel>=0.33.1",
+]
 
-test_deps = ['pytest', 'pytest-cov', 'pytest-raises']
-lint_deps = ['flake8']
-interactive_dev_deps = []
-extras = {
-      'test_group': test_deps,
-      'lint_group': lint_deps,
-      'interactive_dev_group': interactive_dev_deps
+setup_requirements = [
+    "pytest-runner",
+]
+
+interactive_requirements = [
+    "altair",
+    "jupyterlab",
+    "matplotlib",
+    "PIL",
+]
+
+extra_requirements = {
+    "test": test_requirements,
+    "dev": dev_requirements,
+    "setup": setup_requirements,
+    "interactive": interactive_requirements,
+    "all": [
+        *requirements,
+        *test_requirements,
+        *setup_requirements,
+        *dev_requirements,
+        *interactive_requirements
+    ]
 }
 
-setup(name=PACKAGE_NAME,
-      version=MODULE_VERSION,
-      description='A generalized scientific image processing module from the Allen Institute for Cell Science.',
-      long_description=readme(),
-      author='AICS',
-      author_email='!AICS_SW@alleninstitute.org',
-      license='Allen Institute Software License',
-      packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
-      entry_points={
-          "console_scripts": [
-          ]
-      },
-      install_requires=[
-            'imageio>=2.3.0',
-            'numpy>=1.16',
-            'scipy>=1.1.0',
-            'tifffile==0.15.1'
-      ],
-
-      # For test setup. This will allow JUnit XML output for Jenkins
-      setup_requires=['pytest-runner'],
-      tests_require=test_deps,
-      extras_require=extras,
-      zip_safe=False
-      )
+setup(
+    author=(
+        "Dan Toloudis, "
+        "Matthew Bowden, "
+        "Jamie Sherman, "
+        "Jackson Maxfield Brown, "
+        "Zach Crabtree"
+    ),
+    author_email=(
+        "danielt@alleninstitute.org, "
+        "mattb@alleninstitute.org, "
+        "jamies@alleninstitute.org, "
+        "jacksonb@alleninstitute.org, "
+        "zacharyc@alleninstitute.org"
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: Allen Institute Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description=(
+        "Python library for reading and writing image data with special handlers for bio-formats "
+        "from Allen Institute for Cell Science."
+    ),
+    entry_points={},
+    install_requires=requirements,
+    license="Allen Institute Software License",
+    long_description=readme,
+    include_package_data=True,
+    keywords="aicsimageio, allen cell, imaging, computational biology",
+    name="aicsimageio",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
+    python_requires=">=3.6",
+    setup_requires=setup_requirements,
+    test_suite="aicsimageio/tests",
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+    url="https://github.com/AllenCellModeling/aicsimageio",
+    version="3.0.0",
+    zip_safe=False,
+)

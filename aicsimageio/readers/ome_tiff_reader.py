6d91542d82b00d1350f84d2624d7783dc2c288c8
Daniel Toloudis <danielt@alleninstitute.org>
2019-10-30 17:06:31 -0700

    add get_physical_pixel_size to czi and ome tiff readers

--- aicsimageio/readers/ome_tiff_reader.py
+++ aicsimageio/readers/ome_tiff_reader.py
@@ -105,3 +105,7 @@ class OmeTiffReader(Reader):
 
     def get_channel_names(self, scene: int = 0):
         return self.metadata.image(scene).Pixels.get_channel_names()
+
+    def get_physical_pixel_size(self, scene: int = 0):
+        p = self.metadata.image(scene).Pixels
+        return [p.get_PhysicalSizeX(), p.get_PhysicalSizeY(), p.get_PhysicalSizeZ()]

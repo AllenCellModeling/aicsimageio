e0210552d89776a4b34bedcaddd6cdf5b794395a
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-01-26 16:36:25 -0800

    Change the dask utils tests to simply mock the utility function

--- aicsimageio/tests/test_dask_utils.py
+++ aicsimageio/tests/test_dask_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
+from unittest import mock
 
 import pytest
 
@@ -12,50 +12,44 @@ from aicsimageio.readers import CziReader
 @pytest.mark.parametrize("address, nworkers", [
     (None, None),
     (None, 2),
-    pytest.param("tcp://this.will.fail", None, marks=pytest.mark.raises(exception=ValueError))
+    ("tcp://some-spawned-cluster", None),
 ])
 def test_aicsimage_context_manager(resources_dir, address, nworkers):
-    # Load the image in a context manager that spawn and closes a cluster and client
-    with AICSImage(resources_dir / "s_3_t_1_c_3_z_5.czi", address=address, nworkers=nworkers) as img:
-        # We can assert this because by the time we get to this check the cases where we provided a address will have
-        # already failed
-        assert "tcp://127.0.0.1" in img.client.scheduler.address
+    # Patch the spawn function
+    with mock.patch("aicsimageio.dask_utils.spawn_cluster_and_client") as mocked_spawner:
+        return "a", "b"
 
-        # Check that kwargs got passed down
-        if nworkers is not None:
-            assert len(img.cluster.workers) == nworkers
-        else:
-            assert len(img.cluster.workers) >= 1
+        # Patch the shutdown function
+        with mock.patch("aicsimageio.dask_utils.shutdown_cluster_and_client") as mocked_shutdown:
+            return "a", "b"
 
-    # Give some time to the system to fully shutdown the cluster and workers prior to next test
-    time.sleep(5)
+            # Load the image in a context manager that spawn and closes a cluster and client
+            with AICSImage(resources_dir / "s_3_t_1_c_3_z_5.czi", address=address, nworkers=nworkers):
+                assert mocked_spawner.call_args[0][0] == address
+                assert mocked_spawner.call_arg[1]["nworkers"] == nworkers
 
-    # Check that the cluster and client were shutdown after exiting context manager
-    assert img.cluster.status == "closed"
-    assert img.client.status == "closed"
+            # Check that the cluster and client were scheduled to shutdown
+            assert mocked_shutdown.called
 
 
 @pytest.mark.parametrize("address, nworkers", [
     (None, None),
     (None, 2),
-    pytest.param("tcp://this.will.fail", None, marks=pytest.mark.raises(exception=ValueError))
+    ("tcp://some-spawned-cluster", None),
 ])
 def test_reader_context_manager(resources_dir, address, nworkers):
-    # Load the image in a context manager that spawn and closes a cluster and client
-    with CziReader(resources_dir / "s_3_t_1_c_3_z_5.czi", address=address, nworkers=nworkers) as img:
-        # We can assert this because by the time we get to this check the cases where we provided a address will have
-        # already failed
-        assert "tcp://127.0.0.1" in img.client.scheduler.address
+    # Patch the spawn function
+    with mock.patch("aicsimageio.dask_utils.spawn_cluster_and_client") as mocked_spawner:
+        return "a", "b"
 
-        # Check that kwargs got passed down
-        if nworkers is not None:
-            assert len(img.cluster.workers) == nworkers
-        else:
-            assert len(img.cluster.workers) >= 1
+        # Patch the shutdown function
+        with mock.patch("aicsimageio.dask_utils.shutdown_cluster_and_client") as mocked_shutdown:
+            return "a", "b"
 
-    # Give some time to the system to fully shutdown the cluster and workers prior to next test
-    time.sleep(5)
+            # Load the image in a context manager that spawn and closes a cluster and client
+            with CziReader(resources_dir / "s_3_t_1_c_3_z_5.czi", address=address, nworkers=nworkers):
+                assert mocked_spawner.call_args[0][0] == address
+                assert mocked_spawner.call_arg[1]["nworkers"] == nworkers
 
-    # Check that the cluster and client were shutdown after exiting context manager
-    assert img.cluster.status == "closed"
-    assert img.client.status == "closed"
+            # Check that the cluster and client were scheduled to shutdown
+            assert mocked_shutdown.called

aaf3e76ff960aa9f0a0eec7c0d7e97786bafcc0e
heeler <jamie.sherman@gmail.com>
2020-03-31 12:47:44 -0700

    fixed typo in upload script. added back tests for large file

--- aicsimageio/tests/readers/test_lif_reader.py
+++ aicsimageio/tests/readers/test_lif_reader.py
@@ -28,7 +28,7 @@ from aicsimageio.readers.lif_reader import LifReader
             "STCZYX",
             np.uint16,
             0,
-            ("Z", "Y", "X"),
+            ["Z", "Y", "X"],
             (1, 1, 1, 1, 2048, 2048),
             4  # 1 * 1 * 2 * 2 = 4
         ),
@@ -38,31 +38,31 @@ from aicsimageio.readers.lif_reader import LifReader
             "STCZYX",
             np.uint16,
             0,
-            ("Z", "Y", "X"),
+            ["Z", "Y", "X"],
             (1, 1, 1, 1, 614, 614),
             16  # 1 * 4 * 2 * 2 = 16
         ),
         #  To be added back in when rebased off jackson's S3 pr
-        # (
-        #     "s14_t1_c2_z52_inconsistent.lif",
-        #     (1, 1, 2, 38, 2048, 2048),
-        #     "STCZYX",
-        #     np.uint16,
-        #     0,
-        #     ("Z", "Y", "X"),
-        #     (1, 1, 1, 38, 2048, 2048),
-        #     4  # 1 * 1 * 2 * 2 = 4
-        # ),
-        # (
-        #     "s14_t1_c2_z52_inconsistent.lif",
-        #     (1, 1, 2, 52, 2048, 2048),
-        #     "STCZYX",
-        #     np.uint16,
-        #     1,
-        #     ("C", "Y", "X"),
-        #     (1, 1, 2, 1, 2048, 2048),
-        #     104  # 1 * 1 * 52 * 2 = 104
-        # ),
+        (
+            "s_14_t_1_c_2_variable_dims.lif",
+            (1, 1, 2, 38, 2048, 2048),
+            "STCZYX",
+            np.uint16,
+            0,
+            ["Z", "Y", "X"],
+            (1, 1, 1, 38, 2048, 2048),
+            4  # 1 * 1 * 2 * 2 = 4
+        ),
+        (
+            "s_14_t_1_c_2_variable_dims.lif",
+            (1, 1, 2, 52, 2048, 2048),
+            "STCZYX",
+            np.uint16,
+            1,
+            ["C", "Y", "X"],
+            (1, 1, 2, 1, 2048, 2048),
+            104  # 1 * 1 * 52 * 2 = 104
+        ),
     ]
 )
 def test_lif_reader(
@@ -125,7 +125,7 @@ def test_is_this_type(raw_bytes, expected):
 @pytest.mark.parametrize("filename, scene, expected", [
     ("s_1_t_1_c_2_z_1.lif", 0, ["Gray--TL-BF--EMP_BF", "Green--FLUO--GFP"]),
     ("s_1_t_4_c_2_z_1.lif", 0, ["Gray--TL-PH--EMP_BF", "Green--FLUO--GFP"]),
-    #  ("s14_t1_c2_z52_inconsistent.lif", 0, ["Gray--TL-BF--EMP_BF", "Green--FLUO--GFP"])
+    ("s_14_t_1_c_2_variable_dims.lif", 0, ["Gray--TL-BF--EMP_BF", "Green--FLUO--GFP"]),
     pytest.param("s_1_t_1_c_2_z_1.lif", 2, None, marks=pytest.mark.raises(exception=IndexError))
 ])
 def test_get_channel_names(resources_dir, filename, scene, expected):
@@ -135,7 +135,7 @@ def test_get_channel_names(resources_dir, filename, scene, expected):
 @pytest.mark.parametrize("filename, scene, expected", [
     ("s_1_t_1_c_2_z_1.lif", 0, (0.325, 0.325, 1.0)),
     ("s_1_t_4_c_2_z_1.lif", 0, (0.33915, 0.33915, 1.0)),
-    #  ("s14_t1_c2_z52_inconsistent.lif", 0, (0.1625, 0.1625, 1.000715))
+    ("s_14_t_1_c_2_variable_dims.lif", 0, (0.1625, 0.1625, 1.000715))
 ])
 def test_get_physical_pixel_size(resources_dir, filename, scene, expected):
     assert LifReader(resources_dir / filename).get_physical_pixel_size(scene) == pytest.approx(expected, rel=0.001)
@@ -165,25 +165,8 @@ def test_size_functions(resources_dir, filename, s, t, c, z, y, x):
     assert img.size_x() == x
 
 
-# This test doesn't work currently => I'm trying to figure out why but no good guesses yet
-
-# @pytest.mark.parametrize("filename, scene, expected", [
-#     ("s1_t4_c2_z1.lif", 0, (25575, 25009, 25239, 25014, 25288, 25912, 25545, 25067, 25322, 25211, 25204)),
-#     #  ("s14_t1_c2_z52_inconsistent.lif", 0, (0.1625, 0.1625, 1.000715))
-# ])
-# def test_lif_image_data_one(resources_dir, filename, scene, expected):
-#     f = resources_dir / filename
-#
-#     img = LifReader(f)
-#     data_y = img.data[0, 0, 0, 0, 0:10, 0]
-#     data_x = img.data[0, 0, 0, 0, 0, 0:10]
-#     assert np.array_equal(data_y, expected)
-#     assert np.array_equal(data_x, expected)
-
-
 @pytest.mark.parametrize("filename, scene, expected", [
     ("s_1_t_4_c_2_z_1.lif", 0, 51221),
-    #  ("s14_t1_c2_z52_inconsistent.lif", 0, (0.1625, 0.1625, 1.000715))
 ])
 def test_lif_image_data_two(resources_dir, filename, scene, expected):
     f = resources_dir / filename

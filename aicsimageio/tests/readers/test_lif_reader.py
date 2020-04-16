5ea8497d91234f05bf3cb7a34e2bb4aa1df1293d
heeler <jamie.sherman@gmail.com>
2020-04-15 22:26:29 -0700

    updated tests

--- aicsimageio/tests/readers/test_lif_reader.py
+++ aicsimageio/tests/readers/test_lif_reader.py
@@ -145,8 +145,8 @@ def test_get_channel_names(resources_dir, filename, scene, expected):
 
 @pytest.mark.parametrize("filename, scene, expected", [
     ("s_1_t_1_c_2_z_1.lif", 0, (0.325, 0.325, 1.0)),
-    ("s_1_t_4_c_2_z_1.lif", 0, (0.33915, 0.33915, 1.0)),
-    ("s_14_t_1_c_2_variable_dims.lif", 0, (0.1625, 0.1625, 1.000715))
+    ("s_1_t_4_c_2_z_1.lif", 0, (0.33914910277324634, 0.33914910277324634, 1.0)),
+    ("s_14_t_1_c_2_variable_dims.lif", 0, (0.1625, 0.1625, 1.000715)),
 ])
 def test_get_physical_pixel_size(resources_dir, filename, scene, expected):
     f = resources_dir / filename

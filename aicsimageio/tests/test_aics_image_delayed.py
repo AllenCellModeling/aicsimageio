228c1c71de7694c3c7412b575e9522cace702925
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-05-01 22:20:39 -0700

    Use length of task graph rather than running and checking after

--- aicsimageio/tests/test_aics_image_delayed.py
+++ aicsimageio/tests/test_aics_image_delayed.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from dask.diagnostics import Profiler
+from dask import optimize
 from psutil import Process
 
-from aicsimageio import dask_utils, imread
+from aicsimageio import dask_utils, imread_dask
 
 # Example files
 BMP_FILE = "example.bmp"
@@ -23,25 +23,24 @@ BIG_LIF_FILE = "s_1_t_4_c_2_z_1.lif"
 TXT_FILE = "example.txt"
 
 
-with dask_utils.cluster_and_client(processes=False) as (cluster, client):
-
-    @pytest.mark.parametrize(
-        "filename, expected_shape, expected_tasks",
-        [
-            (PNG_FILE, (1, 1, 4, 1, 800, 537), 2),
-            (TIF_FILE, (1, 1, 1, 1, 325, 475), 2),
-            (OME_FILE, (1, 1, 1, 1, 325, 475), 2),
-            (CZI_FILE, (1, 1, 1, 1, 325, 475), 2),
-            (LIF_FILE, (1, 1, 2, 1, 2048, 2048), 4),
-            (MED_TIF_FILE, (1, 10, 3, 1, 325, 475), 60),
-            (BIG_OME_FILE, (3, 1, 3, 5, 325, 475), 90),
-            (BIG_CZI_FILE, (3, 1, 3, 5, 325, 475), 18),
-            (BIG_LIF_FILE, (1, 4, 2, 1, 614, 614), 16),
-        ],
-    )
-    def test_imread_from_delayed(
-        resources_dir, filename, expected_shape, expected_tasks
-    ):
+@pytest.mark.parametrize(
+    "filename, expected_shape, expected_tasks",
+    [
+        (PNG_FILE, (1, 1, 4, 1, 800, 537), 3),
+        (TIF_FILE, (1, 1, 1, 1, 325, 475), 2),
+        (OME_FILE, (1, 1, 1, 1, 325, 475), 2),
+        (CZI_FILE, (1, 1, 1, 1, 325, 475), 2),
+        (LIF_FILE, (1, 1, 2, 1, 2048, 2048), 4),
+        (MED_TIF_FILE, (1, 10, 3, 1, 325, 475), 60),
+        (BIG_OME_FILE, (3, 1, 3, 5, 325, 475), 90),
+        (BIG_CZI_FILE, (3, 1, 3, 5, 325, 475), 18),
+        (BIG_LIF_FILE, (1, 4, 2, 1, 614, 614), 16),
+    ],
+)
+def test_imread_from_delayed(resources_dir, filename, expected_shape, expected_tasks):
+    # Load image as delayed dask array then as numpy array
+    # Check computed task count
+    with dask_utils.cluster_and_client(processes=False) as (cluster, client):
         # Get filepath
         f = resources_dir / filename
 
@@ -49,12 +48,10 @@ with dask_utils.cluster_and_client(processes=False) as (cluster, client):
         proc = Process()
         assert str(f) not in [f.path for f in proc.open_files()]
 
-        # Load image as delayed dask array then as numpy array
-        # Check computed task count
-        with Profiler() as prof:
-            img = imread(f)
-            assert img.shape == expected_shape
-            assert len(prof.results) == expected_tasks
+        # Read image
+        img = imread_dask(f)
+        assert img.shape == expected_shape
+        assert len(optimize(img)[0].__dask_graph__()) == expected_tasks
 
-        # Check that there are no open file pointers after basics
-        assert str(f) not in [f.path for f in proc.open_files()]
+    # Check that there are no open file pointers after basics
+    assert str(f) not in [f.path for f in proc.open_files()]

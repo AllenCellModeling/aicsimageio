98c1d71b6ce83e620caa16ea59d5b0f5920dc84e
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-06-11 16:27:57 -0700

    feature/enable-disable-dask (#124)
    * Add enable, disable dask reading functionality to base reader
    
    * Add tests for disabling dask
    
    * Add comment to README about disabling dask
    
    * Fix lint errors
    
    * Remove contraction in README
    
    * Remove print statement used for testing
    
    * Black formatting
    
    * Feedback from team
    
    * Update README comment
    
    * No need for global declaration

--- aicsimageio/__init__.py
+++ aicsimageio/__init__.py
@@ -4,6 +4,7 @@
 from .aics_image import AICSImage  # noqa: F401
 from .aics_image import imread  # noqa: F401
 from .aics_image import imread_dask  # noqa: F401
+from .readers import use_dask  # noqa: F401
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
--- aicsimageio/tests/test_aics_image_delayed.py
+++ aicsimageio/tests/test_aics_image_delayed.py
@@ -3,9 +3,10 @@
 
 import pytest
 from dask import optimize
+from distributed import get_client
 from psutil import Process
 
-from aicsimageio import dask_utils, imread_dask
+from aicsimageio import AICSImage, dask_utils, imread_dask, use_dask
 
 # Example files
 BMP_FILE = "example.bmp"
@@ -52,6 +53,35 @@ def test_imread_from_delayed(resources_dir, filename, expected_shape, expected_t
         img = imread_dask(f)
         assert img.shape == expected_shape
         assert len(optimize(img)[0].__dask_graph__()) == expected_tasks
+
+    # Check that there are no open file pointers after basics
+    assert str(f) not in [f.path for f in proc.open_files()]
+
+
+def test_imread_use_dask_false(resources_dir):
+    # Load image as delayed dask array then as numpy array
+    # Check computed task count
+    with dask_utils.cluster_and_client(processes=False) as (cluster, client):
+        # Get filepath
+        f = resources_dir / BIG_OME_FILE
+
+        # Check that there are no open file pointers after init
+        proc = Process()
+        assert str(f) not in [f.path for f in proc.open_files()]
+
+        # Check that a client does exist
+        get_client()
+
+        # Don't use dask for reads
+        use_dask(False)
+
+        # Read image without dask
+        img = AICSImage(f)
+        assert img.data.shape == (3, 1, 3, 5, 325, 475)
+
+        # Check that the file was read with base reader then rechunked with dask
+        # Normally the task count for this file is 90
+        assert len(optimize(img.dask_data)[0].__dask_graph__()) == 3
 
     # Check that there are no open file pointers after basics
     assert str(f) not in [f.path for f in proc.open_files()]

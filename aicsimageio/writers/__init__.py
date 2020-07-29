5192cd89dcdbf568e0ba6540274255a642543f2f
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-07-29 15:25:05 -0700

    Remove everything

--- aicsimageio/writers/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from .ome_tiff_writer import OmeTiffWriter  # noqa: F401
-from .rgb_writer import RGBWriter  # noqa: F401
-from .timeseries_writer import TimeseriesWriter  # noqa: F401

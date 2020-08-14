3033cf6d3cc57a01c64c08a06924ccf08792fdb3
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-08-14 11:05:21 -0700

    Include DimensionNames and dimension object discussion

--- aicsimageio/dimensions.py
+++ aicsimageio/dimensions.py
@@ -5,6 +5,41 @@ from typing import Tuple
 
 ###############################################################################
 
+class DimensionNames:
+    Scene = "S"
+    Time = "T"
+    Channel = "C"
+    SpatialZ = "Z"
+    SpatialY = "Y"
+    SpatialX = "X"
+
+DEFAULT_DIMENSION_ORDER = (
+    f"{DimensionNames.Scene}{DimensionNames.Time}{DimensionNames.Channel}"
+    f"{DimensionNames.SpatialZ}{DimensionNames.SpatialY}{DimensionNames.SpatialX}"
+)
+
+###############################################################################
+
+# We don't currently have this problem but I don't really like that I have a
+# DimensionNames and a Dimensions object
+#
+# We could potentially use a "Dimension" NamedTuple to track "name" and "size"
+# This would alleviate the problem of two lookup tables
+# Dimensions.Scene vs Dimensions.S
+# Dimensions.Scene.short == "S"
+# Dimensions.Scene.size == int
+# vs
+# Dimensions.S.name == "Scene"
+# Dimensions.S.size == int
+#
+# This is more verbose than current Dimensions object
+# Dimensions.S == int
+# but may be valuable
+#
+# Just thinking out loud here
+
+###############################################################################
+
 class Dimensions:
 
     def __init__(self, dims: str, shape: Tuple[int]):

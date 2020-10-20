82286cda049c119c088849954ded78151bc3b047
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-10-19 22:49:31 -0700

    Add and update types

--- aicsimageio/types.py
+++ aicsimageio/types.py
@@ -7,12 +7,14 @@ from typing import NamedTuple, Union
 
 import dask.array as da
 import numpy as np
+import xarray as xr
 
 # IO Types
+FSSpecBased = "fsspec-based"
 PathLike = Union[str, Path]
 BytesLike = Union[bytes, BufferedIOBase]
 FileLike = Union[PathLike, BytesLike]
-ArrayLike = Union[np.ndarray, da.Array]
+ArrayLike = Union[np.ndarray, da.Array, xr.DataArray]
 ImageLike = Union[FileLike, ArrayLike]
 
 

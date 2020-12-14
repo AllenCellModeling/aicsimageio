ad2a070b026d8075b42b0aebe624a078e17c216f
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2020-12-13 22:27:25 -0800

    admin/support-py39 (#169)
    * Build and test on py39
    
    * Update imagecodecs and install deps during build
    
    * Add comment re install on py39 + linux in README
    
    * Dep install to build-master, only run on ubuntu
    
    * Connect linebreaks in dep installs with backslash
    
    * Turn off fast fail and install deps on mac
    
    * Try with imagecodecs-lite
    
    * Fix typo in test and lint workflow
    
    * Attempt to get ubuntu + 3.9 building
    
    * Try base ubuntu (just imagecodecs-lite)
    
    * Try with base imagecodecs
    
    * Revert changes, imagecodecs now installs on py39

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
     "dask>=2.9.0",
     "distributed>=2.9.3",
     "numpy>=1.16",
-    "imagecodecs>=2020.2.18",
+    "imagecodecs>=2020.5.30",
     "imageio>=2.3.0",
     "readlif>=0.2.1",
     "lxml>=4.4.2",
@@ -106,6 +106,7 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description=(
         "Delayed parallel image reading, metadata parsing, and image writing for "
@@ -120,7 +121,7 @@ setup(
     keywords="aicsimageio, allen cell, imaging, computational biology",
     name="aicsimageio",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     setup_requires=setup_requirements,
     test_suite="aicsimageio/tests",
     tests_require=test_requirements,

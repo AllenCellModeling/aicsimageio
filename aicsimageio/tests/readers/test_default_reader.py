30cc095629b142f40cbc489ab5bfbecd06b9bdb8
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-12-24 23:36:26 -0800

    Fix error made by merge commit in default reader tests

--- aicsimageio/tests/readers/test_default_reader.py
+++ aicsimageio/tests/readers/test_default_reader.py
@@ -42,6 +42,7 @@ from .reader_test_utils import run_image_read_checks
             "example.png",
             "Image:1",
             None,
+            None,
             marks=pytest.mark.raises(exception=IndexError),
         ),
     ],

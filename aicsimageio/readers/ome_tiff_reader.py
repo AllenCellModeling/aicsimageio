fa685b9cbb65d917feca45c11146dc0d82cd3fc8
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-01-09 14:18:44 -0800

    Migrate OME xml cleaning function to meta utils

--- aicsimageio/readers/ome_tiff_reader.py
+++ aicsimageio/readers/ome_tiff_reader.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from copy import deepcopy
 import logging
-from typing import Dict, List, Tuple, Union
-import re
 import xml.etree.ElementTree as ET
+from typing import Dict, List, Tuple, Union
 
-from fsspec.spec import AbstractFileSystem
 import numpy as np
+import xarray as xr
+from fsspec.spec import AbstractFileSystem
 from ome_types import from_xml
 from ome_types.model.ome import OME
 from tifffile import TiffFile, TiffFileError, TiffTag
-import xarray as xr
 
 from .. import constants, exceptions, transforms, types
-from ..dimensions import DimensionNames, DEFAULT_DIMENSION_ORDER
+from ..dimensions import DEFAULT_DIMENSION_ORDER, DimensionNames
+from ..metadata import utils as metadata_utils
+from ..types import PhysicalPixelSizes
 from ..utils import io_utils
 from .tiff_reader import TiffReader
-from ..types import PhysicalPixelSizes
-from ..metadata import utils as metadata_utils
-
 
 ###############################################################################
 
@@ -32,265 +29,13 @@ log = logging.getLogger(__name__)
 
 ###############################################################################
 
-# This is a known issue that could have been caused by prior versions of aicsimageio
-# due to our old OMEXML.py file.
-#
-# You can see the PR that updated this exact line here:
-# https://github.com/AllenCellModeling/aicsimageio/pull/116/commits/e3f9cde7f680edeef3ef3586a67fd8106e746167#diff-46a483e94af833f7eaa1106921191fed5e7c77f33a5c0c47a8f5a2d35ad3ba96L47
-#
-# Notably why this is invalid is that the 2012-03 schema _doesn't exist_
-#
-# Don't know how this wasn't ever caught before that PR but to ensure that we don't
-# error in reading the OME in aicsimageio>=4.0.0, we manually find and replace this
-# line in OME xml prior to creating the OME object.
-KNOWN_INVALID_OME_XSD_REFERENCES = [
-    "www.openmicroscopy.org/Schemas/ome/2013-06",
-    "www.openmicroscopy.org/Schemas/OME/2012-03",
-]
-REPLACEMENT_OME_XSD_REFERENCE = "www.openmicroscopy.org/Schemas/OME/2016-06"
-
-###############################################################################
-
 
 class OmeTiffReader(TiffReader):
-    @staticmethod
-    def _clean_ome_xml_for_known_issues(xml: str) -> str:
-        # Store list of changes to print out with warning
-        metadata_changes = []
-
-        # Fix xsd reference
-        # This is from OMEXML object just having invalid reference
-        for known_invalid_ref in KNOWN_INVALID_OME_XSD_REFERENCES:
-            if known_invalid_ref in xml:
-                xml = xml.replace(
-                    known_invalid_ref,
-                    REPLACEMENT_OME_XSD_REFERENCE,
-                )
-                metadata_changes.append(
-                    f"Replaced '{known_invalid_ref}' with "
-                    f"'{REPLACEMENT_OME_XSD_REFERENCE}'."
-                )
-
-        # Read in XML
-        root = ET.fromstring(xml)
-
-        # Get the namespace
-        # In XML etree this looks like
-        # "{http://www.openmicroscopy.org/Schemas/OME/2016-06}"
-        # and must prepend any etree finds
-        namespace_matches = re.match(r"\{.*\}", root.tag)
-        if namespace_matches is not None:
-            namespace = namespace_matches.group(0)
-        else:
-            raise ValueError("XML does not contain a namespace")
-
-        # Find all Image elements and fix IDs
-        # This is for certain for test files of o.urs and ACTK files
-        for image_index, image in enumerate(root.findall(f"{namespace}Image")):
-            image_id = image.get("ID")
-            if not image_id.startswith("Image"):
-                image.set("ID", metadata_utils.generate_ome_image_id(image_id))
-                metadata_changes.append(
-                    f"Updated attribute 'ID' from '{image_id}' to 'Image:{image_id}' "
-                    f"on Image element at position {image_index}"
-                )
-
-            # Find all Pixels elements and fix IDs
-            for pixels_index, pixels in enumerate(image.findall(f"{namespace}Pixels")):
-                pixels_id = pixels.get("ID")
-                if not pixels_id.startswith("Pixels"):
-                    pixels.set("ID", f"Pixels:{pixels_id}")
-                    metadata_changes.append(
-                        f"Updated attribute 'ID' from '{pixels_id}' to "
-                        f"Pixels:{pixels_id}' on Pixels element at "
-                        f"position {pixels_index}."
-                    )
-
-                # Determine if there is an out-of-order channel / plane elem
-                # This is due to OMEXML "add channel" function
-                # That added Channels and appropriate Planes to the XML
-                # But, placed them in:
-                # Channel
-                # Plane
-                # Plane
-                # ...
-                # Channel
-                # Plane
-                # Plane
-                #
-                # Instead of grouped together:
-                # Channel
-                # Channel
-                # ...
-                # Plane
-                # Plane
-                # ...
-                #
-                # This effects all CFE files (new and old) but for different reasons
-                pixels_children_out_of_order = False
-                encountered_something_besides_channel = False
-                for child in pixels:
-                    if child.tag != f"{namespace}Channel":
-                        encountered_something_besides_channel = True
-                    if (
-                        encountered_something_besides_channel
-                        and child.tag == f"{namespace}Channel"
-                    ):
-                        pixels_children_out_of_order = True
-                        break
-
-                # Ensure order of:
-                # channels -> bindata | tiffdata | metadataonly -> planes
-                if pixels_children_out_of_order:
-                    # Get all relevant elems
-                    channels = [
-                        deepcopy(c) for c in pixels.findall(f"{namespace}Channel")
-                    ]
-                    bin_data = [
-                        deepcopy(b) for b in pixels.findall(f"{namespace}BinData")
-                    ]
-                    tiff_data = [
-                        deepcopy(t) for t in pixels.findall(f"{namespace}TiffData")
-                    ]
-                    # There should only be one metadata only element but to standardize
-                    # list comprehensions later we findall
-                    metadata_only = [
-                        deepcopy(m) for m in pixels.findall(f"{namespace}MetadataOnly")
-                    ]
-                    planes = [deepcopy(p) for p in pixels.findall(f"{namespace}Plane")]
-
-                    # Old (2018 ish) cell feature explorer files sometimes contain both
-                    # an empty metadata only element and filled tiffdata elements
-                    # Since the metadata only elements are empty we can check this and
-                    # choose the tiff data elements instead
-                    #
-                    # First check if there are any metadata only elements
-                    if len(metadata_only) == 1:
-                        # Now check if _one of_ of the other two choices are filled
-                        # ^ in Python is XOR
-                        if (len(bin_data) > 0) ^ (len(tiff_data) > 0):
-                            metadata_children = list(metadata_only[0])
-                            # Now check if the metadata only elem has no children
-                            if len(metadata_children) == 0:
-                                # If so, just "purge" by creating empty list
-                                metadata_only = []
-
-                            # If there are children elements
-                            # Return XML and let XMLSchema Validation show error
-                            else:
-                                return xml
-
-                    # After cleaning metadata only, validate the normal behaviors of
-                    # OME schema
-                    #
-                    # Validate that there is only one of bindata, tiffdata, or metadata
-                    if len(bin_data) > 0:
-                        if len(tiff_data) == 0 and len(metadata_only) == 0:
-                            selected_choice = bin_data
-                        else:
-                            # Return XML and let XMLSchema Validation show error
-                            return xml
-                    elif len(tiff_data) > 0:
-                        if len(bin_data) == 0 and len(metadata_only) == 0:
-                            selected_choice = tiff_data
-                        else:
-                            # Return XML and let XMLSchema Validation show error
-                            return xml
-                    elif len(metadata_only) == 1:
-                        if len(bin_data) == 0 and len(tiff_data) == 0:
-                            selected_choice = metadata_only
-                        else:
-                            # Return XML and let XMLSchema Validation show error
-                            return xml
-                    else:
-                        # Return XML and let XMLSchema Validation show error
-                        return xml
-
-                    # Remove all children from element to be replaced
-                    # with ordered elements
-                    for elem in list(pixels):
-                        pixels.remove(elem)
-
-                    # Re-attach elements
-                    for channel in channels:
-                        pixels.append(channel)
-                    for elem in selected_choice:
-                        pixels.append(elem)
-                    for plane in planes:
-                        pixels.append(plane)
-
-                    metadata_changes.append(
-                        f"Reordered children of Pixels element at "
-                        f"position {pixels_index}."
-                    )
-
-        # This is a result of dumping basically all experiement metadata
-        # into "StructuredAnnotation" blocks
-        #
-        # This affects new (2020) Cell Feature Explorer files
-        #
-        # Because these are structured annotations we don't want to mess with anyones
-        # besides the AICS generated bad structured annotations
-        aics_anno_removed_count = 0
-        sa = root.find(f"{namespace}StructuredAnnotations")
-        if sa is not None:
-            for xml_anno in sa.findall(f"{namespace}XMLAnnotation"):
-                # At least these are namespaced
-                if xml_anno.get("Namespace") == "alleninstitute.org/CZIMetadata":
-                    # Get ID because some elements have annotation refs
-                    # in both the base Image element and all plane elements
-                    aics_anno_id = xml_anno.get("ID")
-                    for image in root.findall(f"{namespace}Image"):
-                        for anno_ref in image.findall(f"{namespace}AnnotationRef"):
-                            if anno_ref.get("ID") == aics_anno_id:
-                                image.remove(anno_ref)
-
-                        # Clean planes
-                        pixels = image.find(f"{namespace}Pixels")
-                        for plane in pixels.findall(f"{namespace}Plane"):
-                            for anno_ref in plane.findall(f"{namespace}AnnotationRef"):
-                                if anno_ref.get("ID") == aics_anno_id:
-                                    plane.remove(anno_ref)
-
-                    # Remove the whole etree
-                    sa.remove(xml_anno)
-                    aics_anno_removed_count += 1
-
-        # Log changes
-        if aics_anno_removed_count > 0:
-            metadata_changes.append(
-                f"Removed {aics_anno_removed_count} AICS generated XMLAnnotations."
-            )
-
-        # If there are no annotations in StructuredAnnotations, remove it
-        if sa is not None:
-            if len(list(sa)) == 0:
-                root.remove(sa)
-
-        # If any piece of metadata was changed alert and rewrite
-        if len(metadata_changes) > 0:
-            log.debug(
-                "OME metadata was cleaned for known AICSImageIO 3.x OMEXML errors."
-            )
-            log.debug(f"Full list of OME cleaning changes: {metadata_changes}")
-
-            # Register namespace
-            ET.register_namespace("", f"http://{REPLACEMENT_OME_XSD_REFERENCE}")
-
-            # Write out cleaned XML to string
-            xml = ET.tostring(
-                root,
-                encoding="unicode",
-                method="xml",
-            )
-
-        return xml
-
     @staticmethod
     def _get_ome(ome_xml: str, clean_metadata: bool = True) -> OME:
         # To clean or not to clean, that is the question
         if clean_metadata:
-            ome_xml = OmeTiffReader._clean_ome_xml_for_known_issues(ome_xml)
+            ome_xml = metadata_utils.clean_ome_xml_for_known_issues(ome_xml)
 
         return from_xml(ome_xml)
 

7fc9289a696c3af4758cc62d65cbd04e8e6aeaa0
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-01-20 22:32:43 -0800

    Expose all imread options from base import

--- aicsimageio/__init__.py
+++ aicsimageio/__init__.py
@@ -4,6 +4,8 @@
 from .aics_image import AICSImage  # noqa: F401
 from .aics_image import imread  # noqa: F401
 from .aics_image import imread_dask  # noqa: F401
+from .aics_image import imread_xarray  # noqa: F401
+from .aics_image import imread_xarray_dask  # noqa: F401
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md

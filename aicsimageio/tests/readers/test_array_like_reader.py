afc89154defd00f90a8d538f4b0c173f8b7ca535
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-01-30 22:18:49 -0800

    Update tests for array like reader

--- aicsimageio/tests/readers/test_array_like_reader.py
+++ aicsimageio/tests/readers/test_array_like_reader.py
@@ -5,10 +5,10 @@ import dask.array as da
 import numpy as np
 import pytest
 
-from aicsimageio import exceptions
+from aicsimageio import dimensions, exceptions
 from aicsimageio.readers import ArrayLikeReader
 
-from ..image_container_test_utils import check_can_serialize_image_container
+from ..image_container_test_utils import run_image_container_checks
 
 
 @pytest.mark.parametrize(
@@ -19,58 +19,94 @@ from ..image_container_test_utils import check_can_serialize_image_container
     ],
 )
 @pytest.mark.parametrize(
-    "arr_shape, expected_shape, expected_dims, expected_channel_names",
+    "arr_shape, known_dims, expected_shape, expected_dims, expected_channel_names",
     [
-        ((1, 1), (1, 1), "YX", None),
-        ((1, 1, 1), (1, 1, 1), "ZYX", None),
-        ((1, 1, 1, 1), (1, 1, 1, 1), "CZYX", ["Channel:0"]),
+        ((1, 1), None, (1, 1), "YX", None),
+        ((1, 1, 1), None, (1, 1, 1), "ZYX", None),
+        (
+            (1, 1, 1, 1),
+            None,
+            (1, 1, 1, 1),
+            "CZYX",
+            ["Channel:0"],
+        ),
+        (
+            (1, 1, 1, 1, 1),
+            None,
+            (1, 1, 1, 1, 1),
+            dimensions.DEFAULT_DIMENSION_ORDER,
+            ["Channel:0"],
+        ),
+        ((1, 1, 1), "AYX", (1, 1, 1), "AYX", None),
+        (
+            (1, 1, 1, 1),
+            "ZCYX",
+            (1, 1, 1, 1),
+            "ZCYX",
+            ["Channel:0"],
+        ),
+        (
+            (1, 1, 1, 1, 1),
+            "ABCYX",
+            (1, 1, 1, 1, 1),
+            "ABCYX",
+            ["Channel:0"],
+        ),
+        (
+            (3, 2, 5, 4, 10, 10),
+            "RTCZYX",
+            (3, 2, 5, 4, 10, 10),
+            "RTCZYX",
+            ["Channel:0", "Channel:1", "Channel:2", "Channel:3", "Channel:4"],
+        ),
+        # Test that without known dims and with more than five dims, it raises an error
+        # Our guess dim order only support up to five dims
+        pytest.param(
+            (1, 2, 3, 4, 5, 6),
+            None,
+            None,
+            None,
+            None,
+            marks=pytest.mark.raises(
+                exceptions=exceptions.InvalidDimensionOrderingError
+            ),
+        ),
+        pytest.param(
+            "hello world",
+            None,
+            None,
+            None,
+            None,
+            marks=pytest.mark.raises(exceptions=exceptions.UnsupportedFileFormatError),
+        ),
     ],
 )
 def test_arraylike_reader(
-    array_constructor, arr_shape, expected_shape, expected_dims, expected_channel_names
+    array_constructor,
+    arr_shape,
+    known_dims,
+    expected_shape,
+    expected_dims,
+    expected_channel_names,
 ):
     # Construct array
     arr = array_constructor(arr_shape)
 
     # Init
-    image_container = ArrayLikeReader(arr)
+    image_container = ArrayLikeReader(arr, known_dims=known_dims)
 
-    check_can_serialize_image_container(image_container)
-
-    # Set scene
-    image_container.set_scene("Image:0")
-
-    # Check scene info
-    assert image_container.scenes == ("Image:0",)
-    assert image_container.current_scene == "Image:0"
-
-    # Check basics
-    assert image_container.shape == expected_shape
-    assert image_container.dtype == np.float64
-    assert image_container.dims.order == expected_dims
-    assert image_container.dims.shape == expected_shape
-    assert image_container.metadata is None
-    assert image_container.channel_names == expected_channel_names
-    assert image_container.physical_pixel_sizes == (1.0, 1.0, 1.0)
-
-    # Read only a chunk, then read a chunk from the in-memory, compare
-    np.testing.assert_array_equal(
-        image_container.get_image_dask_data("YX").compute(),
-        image_container.get_image_data("YX"),
+    run_image_container_checks(
+        image_container=image_container,
+        set_scene="Image:0",
+        expected_scenes=("Image:0",),
+        expected_current_scene="Image:0",
+        expected_shape=expected_shape,
+        expected_dtype=np.float64,
+        expected_dims_order=expected_dims,
+        expected_channel_names=expected_channel_names,
+        expected_physical_pixel_sizes=(1.0, 1.0, 1.0),
+        expected_metadata_type=type(None),
     )
-
-    # Check that the shape and dtype are expected after reading in full
-    assert image_container.data.shape == expected_shape
-    assert image_container.data.dtype == np.float64
-
-    check_can_serialize_image_container(image_container)
-
-    return image_container
-
-
-def test_invalid_type():
-    with pytest.raises(exceptions.UnsupportedFileFormatError):
-        ArrayLikeReader("hello world")
 
 
 @pytest.mark.parametrize(

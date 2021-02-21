a9edb46be61d5555a6dc364b8fb2fbbe441f234b
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-02-20 22:39:50 -0800

    Working hidden LifReader._get_image_data

--- aicsimageio/readers/lif_reader.py
+++ aicsimageio/readers/lif_reader.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from typing import List, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple
 
 import dask.array as da
 import numpy as np
 import xarray as xr
 from dask import delayed
 from fsspec.spec import AbstractFileSystem
-from readlif.reader import LifFile, LifImage
+from readlif.reader import LifFile
 
-from .. import dimensions, exceptions, types
+from .. import exceptions, types
+from ..dimensions import (
+    DEFAULT_CHUNK_BY_DIMS,
+    DEFAULT_DIMENSION_ORDER_LIST_WITH_MOSAIC_TILES_AND_SAMPLES,
+    DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES,
+    REQUIRED_CHUNK_BY_DIMS,
+    DimensionNames,
+)
 from ..utils import io_utils
 from .reader import Reader
 
@@ -18,11 +25,26 @@ from .reader import Reader
 
 
 class LifReader(Reader):
+    """
+    Wraps the readlif API to provide the same aicsimageio Reader API but for
+    volumetric LIF images.
+
+    Parameters
+    ----------
+    image: types.PathLike
+        Path to image file to construct Reader for.
+    chunk_by_dims: List[str]
+        Which dimensions to create chunks for.
+        Default: DEFAULT_CHUNK_BY_DIMS
+        Note: Dimensions.SpatialY, Dimensions.SpatialX, and DimensionNames.Samples,
+        will always be added to the list if not present during dask array
+        construction.
+    """
+
     @staticmethod
-    def _is_supported_image(fs: AbstractFileSystem, path: str, **kwargs) -> bool:
+    def _is_supported_image(fs: AbstractFileSystem, path: str, **kwargs: Any) -> bool:
         try:
             with fs.open(path) as open_resource:
-                print(type(open_resource))
                 LifFile(open_resource)
                 return True
 
@@ -32,39 +54,24 @@ class LifReader(Reader):
     def __init__(
         self,
         image: types.PathLike,
-        chunk_by_dims: List[str] = dimensions.DEFAULT_CHUNK_BY_DIMS,
+        chunk_by_dims: List[str] = DEFAULT_CHUNK_BY_DIMS,
     ):
-        """
-        Wraps the readlif API to provide the same aicsimageio Reader API but for
-        volumetric LIF images.
-
-        Parameters
-        ----------
-        image: types.PathLike
-            Path to image file to construct Reader for.
-        chunk_by_dims: List[str]
-            Which dimensions to create chunks for.
-            Default: DEFAULT_CHUNK_BY_DIMS
-            Note: Dimensions.SpatialY, Dimensions.SpatialX, and DimensionNames.Samples,
-            will always be added to the list if not present during dask array
-            construction.
-        """
         # Expand details of provided image
-        self.fs, self.path = io_utils.pathlike_to_fs(image, enforce_exists=True)
+        self._fs, self._path = io_utils.pathlike_to_fs(image, enforce_exists=True)
 
         # Store params
         self.chunk_by_dims = chunk_by_dims
 
         # Enforce valid image
-        if not self._is_supported_image(self.fs, self.path):
+        if not self._is_supported_image(self._fs, self._path):
             raise exceptions.UnsupportedFileFormatError(
-                self.__class__.__name__, self.path
+                self.__class__.__name__, self._path
             )
 
     @property
-    def scenes(self) -> Tuple[str]:
+    def scenes(self) -> Tuple[str, ...]:
         if self._scenes is None:
-            with self.fs.open(self.path) as open_resource:
+            with self._fs.open(self._path) as open_resource:
                 lif = LifFile(open_resource)
                 scene_names = [image["name"] for image in lif.image_list]
                 self._scenes = tuple(scene_names)
@@ -76,11 +83,11 @@ class LifReader(Reader):
         fs: AbstractFileSystem,
         path: str,
         scene: int,
-        indices: Tuple[Union[int, slice]],
+        retrieve_indices: Tuple[Optional[int]],
     ) -> np.ndarray:
         """
-        Open a file for reading, pull each plane neccessary to meet the indices
-        requested, stack into an array, reshape, and return.
+        Open a file for reading, construct a Zarr store, select data, and compute to
+        numpy.
 
         Parameters
         ----------
@@ -90,8 +97,9 @@ class LifReader(Reader):
             The path to file to read.
         scene: int
             The scene index to pull the chunk from.
-        indices: Tuple[Union[int, slice]]
+        retrieve_indices: Tuple[Optional[int]],
             The image indices to retrieve.
+            If None, retrieve the whole dimension.
 
         Returns
         -------
@@ -108,18 +116,90 @@ class LifReader(Reader):
         with fs.open(path) as open_resource:
             selected_scene = LifFile(open_resource).get_image(scene)
 
-            # Pull all the desired planes
-            planes: List[np.ndarray] = []
-            for dim, index_operation in zip(
-                dimensions.DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES[:-2], indices[:-2]
+            # Create the fill array shape
+            # Drop the YX
+            retrieve_shape: List[int] = []
+            use_selected_or_np_map: Dict[str, Optional[int]] = {}
+            for dim, index_op in zip(
+                DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES[:-2],
+                retrieve_indices[:-2],
             ):
-                if isinstance(index_operation, int):
-                    # pull plane
-                    planes.append(selected_scene.get_frame(dim=index_operation))
+                # Handle slices
+                if index_op is None:
+                    # Store the dim for later to inform to use the np index
+                    use_selected_or_np_map[dim] = None
+                    if dim == DimensionNames.MosaicTile:
+                        retrieve_shape.append(selected_scene.n_mosaic)
+                    elif dim == DimensionNames.Time:
+                        retrieve_shape.append(selected_scene.nt)
+                    elif dim == DimensionNames.Channel:
+                        retrieve_shape.append(selected_scene.channels)
+                    elif dim == DimensionNames.SpatialZ:
+                        retrieve_shape.append(selected_scene.nz)
 
+                # Handle non-chunk dimensions (specific indices / ints)
                 else:
-                    for single_slice_iter in index_operation:
-                        # pull planes
+                    # Store the dim for later to inform to use the provided index
+                    use_selected_or_np_map[dim] = index_op
+                    retrieve_shape.append(1)
+
+            # Make ndarray for lazy arrays to fill
+            planes: List[np.ndarray] = []
+            np_array_for_indices = np.empty(tuple(retrieve_shape), dtype=object)
+            for np_index, _ in np.ndenumerate(np_array_for_indices):
+                # Get true ops
+                true_ops: Dict[str, int] = {}
+
+                # Handle MosaicTile
+                if use_selected_or_np_map[DimensionNames.MosaicTile] is None:
+                    true_ops["m"] = np_index[
+                        DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES.index(
+                            DimensionNames.MosaicTile
+                        )
+                    ]
+                else:
+                    true_ops["m"] = use_selected_or_np_map[DimensionNames.MosaicTile]
+
+                # Handle Time
+                if use_selected_or_np_map[DimensionNames.Time] is None:
+                    true_ops["t"] = np_index[
+                        DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES.index(
+                            DimensionNames.Time
+                        )
+                    ]
+                else:
+                    true_ops["t"] = use_selected_or_np_map[DimensionNames.Time]
+
+                # Handle Channels
+                if use_selected_or_np_map[DimensionNames.Channel] is None:
+                    true_ops["c"] = np_index[
+                        DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES.index(
+                            DimensionNames.Channel
+                        )
+                    ]
+                else:
+                    true_ops["c"] = use_selected_or_np_map[DimensionNames.Channel]
+
+                # Handle SpatialZ
+                if use_selected_or_np_map[DimensionNames.SpatialZ] is None:
+                    true_ops["z"] = np_index[
+                        DEFAULT_DIMENSION_ORDER_WITH_MOSAIC_TILES.index(
+                            DimensionNames.SpatialZ
+                        )
+                    ]
+                else:
+                    true_ops["z"] = use_selected_or_np_map[DimensionNames.SpatialZ]
+
+                # Fill the numpy array with the delayed arrays
+                planes.append(np.asarray(selected_scene.get_frame(**true_ops)))
+
+            # concatenate and reshape to get rid of the array of arrays
+            scene_dims = selected_scene.info["dims"]
+            retrieved_chunk = np.stack(planes).reshape(
+                np_array_for_indices.shape + (scene_dims.y, scene_dims.x)
+            )
+
+            return retrieved_chunk
 
     def _read_delayed(self) -> xr.DataArray:
         """
@@ -137,7 +217,7 @@ class LifReader(Reader):
             supported.
         """
         # Always add the plane dimensions if not present already
-        for dim in dimensions.REQUIRED_CHUNK_BY_DIMS:
+        for dim in REQUIRED_CHUNK_BY_DIMS:
             if dim not in self.chunk_by_dims:
                 self.chunk_by_dims.append(dim)
 
@@ -145,10 +225,10 @@ class LifReader(Reader):
         self.chunk_by_dims = [d.upper() for d in self.chunk_by_dims]
 
         # Construct the delayed dask array
-        with self.fs.open(self.path) as open_resource:
+        with self._fs.open(self._path) as open_resource:
             selected_scene = LifFile(open_resource).get_image(self.current_scene_index)
             selected_scene_dims = (
-                dimensions.DEFAULT_DIMENSION_ORDER_LIST_WITH_MOSAIC_TILES_AND_SAMPLES
+                DEFAULT_DIMENSION_ORDER_LIST_WITH_MOSAIC_TILES_AND_SAMPLES
             )
 
             # Constuct the chunk and non-chunk shapes one dim at a time
@@ -176,7 +256,7 @@ class LifReader(Reader):
 
             # Make ndarray for lazy arrays to fill
             lazy_arrays = np.ndarray(blocked_shape, dtype=object)
-            for plane_index, (np_index, _) in enumerate(np.ndenumerate(lazy_arrays)):
+            for np_index, _ in np.ndenumerate(lazy_arrays):
                 # All dimensions get their normal index except for chunk dims
                 # which get filled with "full" slices
                 indices_with_slices = np_index[: len(non_chunk_shape)] + (
@@ -186,8 +266,8 @@ class LifReader(Reader):
                 # Fill the numpy array with the delayed arrays
                 lazy_arrays[np_index] = da.from_delayed(
                     delayed(LifReader._get_image_data)(
-                        fs=self.fs,
-                        path=self.path,
+                        fs=self._fs,
+                        path=self._path,
                         scene=self.current_scene_index,
                         indices=indices_with_slices,
                     ),

bea48ae7ff6cc3e5febdeaaeb9684145d5089a5b
heeler <jamie.sherman@gmail.com>
2021-04-30 11:17:21 -0700

    czi optional import

--- aicsimageio/readers/__init__.py
+++ aicsimageio/readers/__init__.py
@@ -2,8 +2,5 @@
 # -*- coding: utf-8 -*-
 
 from .array_like_reader import ArrayLikeReader  # noqa: F401
-from .czi_reader import CziReader  # noqa: F401
-
-# from .lif_reader import LifReader  # noqa: F401  // shouldn't this be here too ?
 from .ome_tiff_reader import OmeTiffReader  # noqa: F401
 from .tiff_reader import TiffReader  # noqa: F401

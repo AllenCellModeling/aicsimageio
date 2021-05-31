dd63ff0dce9c0a36536b08c6e0626f8cccb9b821
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-05-31 09:31:16 -0700

    Bump version: 4.0.0.dev6 → 4.0.0.dev7

--- setup.py
+++ setup.py
@@ -115,6 +115,6 @@ setup(
     url="https://github.com/AllenCellModeling/aicsimageio",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.md
-    version="4.0.0.dev6",
+    version="4.0.0.dev7",
     zip_safe=False,
 )

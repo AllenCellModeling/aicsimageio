9d6da2623c8c8bd9728048714d2c33520d3e943f
Jackson Maxfield Brown <jmaxfieldbrown@gmail.com>
2021-07-14 21:16:53 -0700

    admin/bump-min-tifffile (#285)
    * Create OME and scene Tuple during init
    
    * Add simple property check test on no scene call
    
    * Fix linting
    
    * Bump min tifffile to support micromanager files
    
    * Add tests for multi-file, multi-scene reading
    
    * Update comment for remote reading mm files

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ requirements = [
     "imagecodecs>=2020.5.30",
     "numpy~=1.16",
     "ome-types~=0.2.4",
-    "tifffile>=2021.2.1",
+    "tifffile>=2021.6.6",
     "toolz~=0.11.0",
     "xarray~=0.16.1",
     "xmlschema",  # no pin because it's pulled in from OME types

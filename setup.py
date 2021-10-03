2bd5e38d7fd5b707699878c17073ce849b5c6288
psobolewskiPhD <76622105+psobolewskiPhD@users.noreply.github.com>
2021-10-03 23:24:28 +0200

    admin/bump-readlif-for-multi-scene (#327)
    `Readlif` has been update to better handle scene names that have a parent/child structure.
    This change addresses the issue originally reported here: https://github.com/AllenCellModeling/aicsimageio/issues/277
    Bumping the version requirement on `readlif` is a significant quality-of-life improvement for `aicsimageio` users, as well as the new multi-scene reading widget in `napari-aicsimageio`. For example, in the case of multi-well experiments, regions of interest may have the same names (R1, R1 ...), but the parent (well) names will differ (A1, B2, etc.).

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md") as readme_file:
 
 format_libs: Dict[str, List[str]] = {
     "base-imageio": ["imageio[ffmpeg]>=2.9.0,<3", "Pillow>=8.2.0,!=8.3.0,<9"],
-    "lif": ["readlif>=0.6.1"],
+    "lif": ["readlif>=0.6.4"],
     "czi": ["aicspylibczi>=3.0.2"],
     "bioformats": ["bioformats_jar", "wrapt>=1.12"],
 }

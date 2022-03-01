5f67224b682befcad861c69793babe38e5c35c42
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2022-02-28 20:48:33 -0800

    Bump version: 4.6.0 → 4.6.1

--- aicsimageio/__init__.py
+++ aicsimageio/__init__.py
@@ -13,7 +13,7 @@ __author__ = "Jackson Maxfield Brown, Allen Institute for Cell Science"
 __email__ = "jmaxfieldbrown@gmail.com, jamie.sherman@gmail.com, bowdenm@spu.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "4.6.0"
+__version__ = "4.6.1"
 
 
 def get_module_version() -> str:

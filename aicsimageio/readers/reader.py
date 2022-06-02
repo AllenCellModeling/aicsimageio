e1bd0e322e5654e78b530951ec5679b1d10e0279
John Russell <johncrussell25@gmail.com>
2022-06-02 13:34:42 -0400

    import annotations to pass tests in python 3.8

--- aicsimageio/readers/reader.py
+++ aicsimageio/readers/reader.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Tuple, Union

425f15706833c3fdd8dcb138d573e196ec16d14b
Daniel Toloudis <danielt@alleninstitute.org>
2022-07-18 15:13:19 -0700

    fix typing, use PluginEntry everywhere, and add findReadersForPath

--- aicsimageio/aics_image.py
+++ aicsimageio/aics_image.py
@@ -165,10 +165,10 @@ class AICSImage:
         """
         if isinstance(image, str):
             # try to find by extension
-            readermeta = find_reader_for_path(image)
-            if readermeta is not None:
-                print(f"found plugin reader for {image} : {readermeta}")
-                return readermeta.get_reader()
+            plugin_entry = find_reader_for_path(image)
+            if plugin_entry is not None:
+                print(f"found plugin reader for {image} : {plugin_entry}")
+                return plugin_entry.readermeta.get_reader()
 
         if isinstance(image, list) and isinstance(image[0], (str, Path)):
             return TiffGlobReader
--- aicsimageio/plugins.py
+++ aicsimageio/plugins.py
@@ -19,7 +19,7 @@ class PluginEntry(NamedTuple):
 plugin_cache: List[PluginEntry] = []
 # global cache of plugins by extension
 # note there can be multiple readers for the same extension
-plugins_by_ext: Dict[str, List[ReaderMetadata]] = {}
+plugins_by_ext: Dict[str, List[PluginEntry]] = {}
 
 
 def get_plugins():
@@ -27,16 +27,14 @@ def get_plugins():
     for plugin in plugins:
         # ReaderMetadata knows how to instantiate the actual Reader
         reader_meta = plugin.load().ReaderMetadata
-        plugin_cache.append({
-            "readermeta": reader_meta,
-            "entrypoint": plugin
-        })
+        pluginentry = PluginEntry(plugin, reader_meta)
+        plugin_cache.append(pluginentry)
         exts = reader_meta.get_supported_extensions()
         for ext in exts:
             if ext not in plugins_by_ext:
                 plugins_by_ext[ext] = []
             # TODO insert in sorted order (sorted by most recently installed)
-            plugins_by_ext[ext].append(reader_meta)
+            plugins_by_ext[ext].append(pluginentry)
 
     return plugin_cache
 
@@ -45,13 +43,13 @@ def dump_plugins():
     # TODO don't call get_plugins every time
     get_plugins()
     for plugin in plugin_cache:
-        ep = plugin["entrypoint"]
+        ep = plugin.entrypoint
         print(ep.name)
         print(f"  Author  : {ep.dist.metadata['author']}")
         print(f"  Version : {ep.dist.version}")
         print(f"  License : {ep.dist.metadata['license']}")
         # print(f"  Description : {ep.dist.metadata['description']}")
-        reader_meta = plugin["readermeta"]
+        reader_meta = plugin.readermeta
         exts = ", ".join(reader_meta.get_supported_extensions())
         print(f"  Supported Extensions : {exts}")
     print("Plugins for extensions:")
@@ -61,15 +59,23 @@ def dump_plugins():
         print(f"{ext}: {plugins}")
 
 
-def find_reader_for_path(path: str) -> Optional[ReaderMetadata]:
-    # this is not great and there is probably a cleverer way to do this
-
+def find_reader_for_path(path: str) -> Optional[PluginEntry]:
     # try to match on the longest possible registered extension
     exts = sorted(plugins_by_ext.keys(), key=len, reverse=True)
     for ext in exts:
         if path.endswith(ext):
             candidates = plugins_by_ext[ext]
-            # TODO sort by installed date
+            # TODO select a candidate by some criteria?
             return candidates[0]
     # didn't find a reader for this extension
     return None
+
+
+def find_readers_for_path(path: str) -> List[PluginEntry]:
+    candidates = []
+    # try to match on the longest possible registered extension first
+    exts = sorted(plugins_by_ext.keys(), key=len, reverse=True)
+    for ext in exts:
+        if path.endswith(ext):
+            candidates = candidates + plugins_by_ext[ext]
+    return candidates

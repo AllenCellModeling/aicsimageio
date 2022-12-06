25d561ef7a20b63ab8a87657421550a7e74cdece
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-12-06 12:46:07 -0800

    Bump version: 4.9.3 → 4.9.4

--- setup.py
+++ setup.py
@@ -168,6 +168,6 @@ setup(
     url="https://github.com/AllenCellModeling/aicsimageio",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.md
-    version="4.9.3",
+    version="4.9.4",
     zip_safe=False,
 )

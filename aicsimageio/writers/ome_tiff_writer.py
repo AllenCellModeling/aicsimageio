05bc41c356a9a8add5e698ead5d56f61df9cdfa5
Daniel Toloudis <danielt@alleninstitute.org>
2023-04-03 13:30:30 -0700

    try to use optimized codepath through ome-types

--- aicsimageio/writers/ome_tiff_writer.py
+++ aicsimageio/writers/ome_tiff_writer.py
@@ -255,7 +255,7 @@ class OmeTiffWriter(Writer):
             )
         # else if string, then construct OME from string
         elif isinstance(ome_xml, str):
-            ome_xml = from_xml(ome_xml)
+            ome_xml = from_xml(ome_xml, parser="lxml")
 
         # if we do not have an OME object now, something is wrong
         if not isinstance(ome_xml, OME):

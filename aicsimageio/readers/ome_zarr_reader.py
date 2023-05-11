375b8148128ef2fa4453dda08da17233e1eef8f1
Brian Whitney <brian.whitney@alleninstitute.org>
2023-05-11 15:40:59 -0700

    OmeZarrReader working baseline

--- aicsimageio/readers/ome_zarr_reader.py
+++ aicsimageio/readers/ome_zarr_reader.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import xarray as xr
 from fsspec.implementations.local import LocalFileSystem
 from fsspec.spec import AbstractFileSystem
-from ome_types import OME, from_xml
 
-from .. import constants, exceptions, types
+from .. import exceptions, types
+from ..dimensions import DimensionNames
 from ..metadata import utils as metadata_utils
 from ..utils import io_utils
 from .reader import Reader
@@ -39,19 +39,11 @@ class OmeZarrReader(Reader):
     """
 
     @staticmethod
-    def _get_ome(ome_xml: str, clean_metadata: bool = True) -> OME:
-        # To clean or not to clean, that is the question
-        if clean_metadata:
-            ome_xml = metadata_utils.clean_ome_xml_for_known_issues(ome_xml)
-
-        return from_xml(ome_xml, parser="lxml")
-
-    @staticmethod
     def _is_supported_image(fs: AbstractFileSystem, path: str, **kwargs: Any) -> bool:
         try:
-            with fs.open(path) as open_resource:
-                ZarrReader(parse_url(open_resource, mode="r"))
-                return True
+            _ = fs  # TODO: How to use the FS to test for suppored image
+            ZarrReader(parse_url(path, mode="r"))
+            return True
 
         except ValueError:
             return False
@@ -59,7 +51,7 @@ class OmeZarrReader(Reader):
     def __init__(
         self,
         image: types.PathLike,
-        clean_metadata: bool = True,
+        channel_names: Optional[Union[List[str], List[List[str]]]] = None,
         fs_kwargs: Dict[str, Any] = {},
     ):
         # Expand details of provided image
@@ -81,46 +73,110 @@ class OmeZarrReader(Reader):
                 self.__class__.__name__, self._path
             )
 
-        self.clean_metadata = clean_metadata
+        self._channel_names = channel_names
 
-        # Get ome-types object and warn of other behaviors
-        with ZarrReader(parse_url(self._path, mode="r")).zarr as OmeZarr:
-            self._ome = self._get_ome(
-                OmeZarr.root_attrs["WHATEVER KEY GIVES OME METADATA"],  # OMERO?
-                self.clean_metadata,
+    @property
+    def scenes(self) -> Tuple[str, ...]:
+        if self._scenes is None:
+            OmeZarr = ZarrReader(parse_url(self._path, mode="r")).zarr
+            self._scenes = tuple(
+                metadata_utils.generate_ome_image_id(i)
+                for i in range(len(OmeZarr.root_attrs["multiscales"]))
             )
-
-            self._scenes: Tuple[str, ...] = tuple(
-                image_meta.id for image_meta in self._ome.images
-            )
+        return self._scenes
 
     def _read_delayed(self) -> xr.DataArray:
-        return self._xarr_format(self._path, delayed=True)
+        return self._xarr_format(delayed=True)
 
     def _read_immediate(self) -> xr.DataArray:
-        return self._xarr_format(self._path, delayed=False)
+        return self._xarr_format(delayed=False)
 
-    def _xarr_format(self, path: str, delayed: bool) -> xr.DataArray:
+    def _xarr_format(self, delayed: bool) -> xr.DataArray:
 
-        with ZarrReader(parse_url(path, mode="r")).zarr as OmeZarr:
-            dask_array = OmeZarr.load(str(self.current_scene_index))
-            axes = OmeZarr.root_attrs["multiscales"][self.current_scene_index].axes
-            dims = [sub["name"].upper() for sub in axes]
+        OmeZarr = ZarrReader(parse_url(self._path, mode="r")).zarr
+        image_data = OmeZarr.load(str(self.current_scene_index))
+        axes = OmeZarr.root_attrs["multiscales"][self.current_scene_index]["axes"]
+        dims = [sub["name"].upper() for sub in axes]
 
-            coords = metadata_utils.get_coords_from_ome(
-                ome=self._ome,
-                scene_index=self.current_scene_index,
+        if not delayed:
+            image_data = image_data.compute()
+
+        channels = self._get_channel_names_for_scene(image_data.shape, dims)
+
+        coords = self._get_coords(
+            dims,
+            image_data.shape,
+            scene_index=self.current_scene_index,
+            channel_names=channels,
+        )
+
+        return xr.DataArray(
+            image_data,
+            dims=dims,
+            coords=coords,
+            attrs={
+                # TODO: ADD info from "multiscales" "omero"?
+            },
+        )
+
+    @staticmethod
+    def _get_coords(
+        dims: List[str],
+        shape: Tuple[int, ...],
+        scene_index: int,
+        channel_names: Optional[List[str]],
+    ) -> Dict[str, Any]:
+        # Use dims for coord determination
+        coords: Dict[str, Any] = {}
+
+        if channel_names is None:
+            # Get ImageId for channel naming
+            image_id = metadata_utils.generate_ome_image_id(scene_index)
+
+            # Use range for channel indices
+            if DimensionNames.Channel in dims:
+                coords[DimensionNames.Channel] = [
+                    metadata_utils.generate_ome_channel_id(
+                        image_id=image_id, channel_id=i
+                    )
+                    for i in range(shape[dims.index(DimensionNames.Channel)])
+                ]
+        else:
+            coords[DimensionNames.Channel] = channel_names
+
+        return coords
+
+    def _get_channel_names_for_scene(
+        self, image_shape: Tuple[int], dims: List[str]
+    ) -> Optional[List[str]]:
+        # Fast return in None case
+        if self._channel_names is None:
+            return None
+
+        # If channels was provided as a list of lists
+        if isinstance(self._channel_names[0], list):
+            scene_channels = self._channel_names[self.current_scene_index]
+        elif all(isinstance(c, str) for c in self._channel_names):
+            scene_channels = self._channel_names  # type: ignore
+        else:
+            return None
+
+        # If scene channels isn't None and no channel dimension raise error
+        if DimensionNames.Channel not in dims:
+            raise exceptions.ConflictingArgumentsError(
+                f"Provided channel names for scene with no channel dimension. "
+                f"Scene dims: {dims}, "
+                f"Provided channel names: {scene_channels}"
             )
 
-            if not delayed:
-                dask_array = dask_array.compute()
-
-            return xr.DataArray(
-                dask_array,
-                dims=dims,
-                coords=coords,
-                attrs={
-                    constants.METADATA_PROCESSED: self._ome,
-                    # ADD info from "multiscenes"?
-                },
+        # If scene channels isn't the same length as the size of channel dim
+        if len(scene_channels) != image_shape[dims.index(DimensionNames.Channel)]:
+            raise exceptions.ConflictingArgumentsError(
+                f"Number of channel names provided does not match the "
+                f"size of the channel dimension for this scene. "
+                f"Scene shape: {image_shape}, "
+                f"Dims: {dims}, "
+                f"Provided channel names: {self._channel_names}",
             )
+
+        return scene_channels  # type: ignore

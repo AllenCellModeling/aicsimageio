3a72353808abb2c26ddbf98b54d064fa186963e1
SeanLeRoy <41307451+SeanLeRoy@users.noreply.github.com>
2023-10-30 17:52:12 +0000

    Deploying to gh-pages from main @ 39daa4dfa774bc7ebf2b8e821fe7d2b93b5b0e2a 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.12.1',
+    VERSION: '4.13.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

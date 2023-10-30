8c097bd14d85908bbfa385233ccb2a890169c59e
SeanLeRoy <seanm@alleninstitute.org>
2023-10-30 11:41:14 -0700

    Bump version: 4.13.0 → 4.14.0

--- setup.py
+++ setup.py
@@ -172,6 +172,6 @@ setup(
     url="https://github.com/AllenCellModeling/aicsimageio",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.md
-    version="4.13.0",
+    version="4.14.0",
     zip_safe=False,
 )
